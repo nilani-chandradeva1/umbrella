9a2ee4eb2ef2cf52955a1a9c37aff8b04be65dfa
nilani-chandradeva1 <nilani.chandradeva1@student.lshtm.ac.uk>
2021-05-25 10:50:21 +0100

    made some updates

--- R/fit.R
+++ R/fit.R
@@ -7,6 +7,16 @@
 #'
 #' @return Optim output
 #' @export
+
+require(tidyr)
+rain_dat <- read.csv("../data requests/LisaReimer/rain_raw.csv", header = TRUE)
+rain_dat <- gather(rain_dat, month, rainfall, JAN:DEC, factor_key = TRUE)
+rain_dat$row_num <- seq.int(nrow(rain_dat))
+rain_dat <- subset(rain_dat, !is.na(rainfall))
+nrow(rain_dat) #349 days
+
+target = rain_dat$rainfall
+t = rain_dat$row_num
 fit_season <- function(target, t){
   stats::optim(par = rep(0, 7),
         target = target, t = t,
@@ -14,6 +24,7 @@ fit_season <- function(target, t){
         method = "L-BFGS-B",
         lower = rep(-10, 7), upper = rep(10, 7))
 }
+
 
 #' Fourier fitting function internal
 #'
@@ -32,3 +43,5 @@ season_optim <- function(par, target, t, ss = TRUE){
     return(sum(abs(season_pred - target)))
   }
 }
+
+
